526a2bd459d3a9ac49b4beee956faf6eb2b83623
JamesVitaly <jamesvitaly1993@gmail.com>
2019-05-22 13:21:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ green=`tput setaf 2`
 yellow=`tput setaf 3`
 blue=`tput setaf 4`
 reset=`tput sgr0`
-echo "${yellow}★★★ ${green}Whee, computers! Let's install some software, hombre. ${yellow}★★★${reset}"
+echo "${yellow}★★★ ${green} Let's install some software! ${yellow}★★★${reset}"
 sleep 2
 
 # Prompt for SUDO before starting the waterfall :)
@@ -15,7 +15,7 @@ sudo -l
 
 # Setup OhMyZSH
 curl -L http://install.ohmyz.sh | sh
-echo -e "${green}✓ Blammo! OhMyZSH installed.${reset}"
+echo -e "${green}✓ Epic! OhMyZSH installed.${reset}"
 sleep 2
 
 # Get Homebrew and Node/NPM installed
@@ -27,47 +27,46 @@ brew install git
 brew install git-extras
 brew install yarn
 
-echo -e "${green}✓ Blammo! Homebrew, Node, and NVM installed.${reset}"
+echo -e "${green}✓ Homebrew, Node, and NVM installed.${reset}"
 sleep 2
 
 # Install Cask to automate application installs
 brew tap caskroom/cask
 brew install brew-cask
 brew tap caskroom/versions
-echo -e "${green}✓ Blammo! Cask installed.${reset}"
+echo -e "${green}✓ Cask installed.${reset}"
 sleep 2
 
-echo -e "${green}✓ Blammo! Browsers installed.${reset}"
+brew cask install google-chrome
+
+echo -e "${green}✓ Browsers installed.${reset}"
 sleep 2
 
 # Install applications via Cask
 # Remember some apps are better left managed on the app store
 brew cask install 1password
-brew cask install aerial
 brew cask install airtable
 brew cask install dropbox
-brew cask install fantastical
 brew cask install iterm2
-brew cask install lungo
 brew cask install numi
 brew cask install sketch
-brew cask install spectacle
+brew cask install slack
 brew cask install spotify
 brew cask install zoomus
 
-echo -e "${green}✓ Blammo! OSX applications installed.${reset}"
+echo -e "${green}✓ Awesome OSX applications installed.${reset}"
 sleep 2
 
 # Source symlinks *after* installing dependencies that may error out.
 source symlinks
 
-echo -e "${green}✓ Blammo! Symlinks copied.${reset}"
+echo -e "${green}✓Symlinks copied.${reset}"
 sleep 2
 
 # Install Meteor
 curl https://install.meteor.com/ | sh
 
-echo -e "${green}✓ Blammo! Meteor installed.${reset}"
+echo -e "${green}✓ Meteor installed.${reset}"
 sleep 2
 
 # Install NPM packages.
@@ -77,8 +76,8 @@ npm install -g grunt
 npm install -g grunt-cli
 npm install -g gulp
 
-echo -e "${green}✓ Blammo! NPM packages installed.${reset}"
+echo -e "${green}✓NPM packages installed.${reset}"
 sleep 2
 
 # Complete!
-echo -e "${yellow}★★★ ${green}Done! Hooray for computers! ${yellow}★★★${reset}"
+echo -e "${yellow}★★★ ${green}Done! A whole mornings first from a single script. Nice job :) ! ${yellow}★★★${reset}"
